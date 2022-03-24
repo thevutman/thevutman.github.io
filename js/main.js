1132757ddbbb6bebeb168a8288b45207ade09bf5
Santiago Velasco <santiagovelascomosquera@hotmail.com>
2022-03-24 10:55:03 -0500

    Enlaces listos quedan redes sociales

--- js/main.js
+++ js/main.js
@@ -13,18 +13,22 @@ function desplegable()
 
 var first = document.getElementById('first-content');
 var second = document.getElementById('second-content');
+var third = document.getElementById('third-content')
+var fourth = document.getElementById('fourth-content')
 
-var buttons = {
-    web: document.getElementById('web-botton'),
-    js: document.getElementById('js-botton')
-}
+var web = document.getElementById('web-botton')
+var js = document.getElementById('js-botton')
+var contact = document.getElementById('contact-botton')
+var aboutme = document.getElementById('aboutme-botton')
 
 
 var main = document.getElementById("main");
 var home = document.getElementById("home-icon")
 
-buttons.web.addEventListener("click", openweb);
-buttons.js.addEventListener("click", openjs);
+web.addEventListener("click", openweb);
+js.addEventListener("click", openjs);
+contact.addEventListener("click", opencontact)
+aboutme.addEventListener("click", openaboutme)
 home.addEventListener("click", openhome);
 
 function openweb()
@@ -32,8 +36,14 @@ function openweb()
         main.classList.add('main-open');
         first.classList.remove('openjs');
         second.classList.add('openweb');
+        third.classList.remove('opencontact')
+        fourth.classList.remove('openaboutme')
         change.classList.toggle("menu-active");
         menu.classList.remove("menu-icon-active");
+        js.style.color="white"
+        web.style.color="gray"
+        aboutme.style.color="white"
+        contact.style.color="white"
 }
 
 function openjs()
@@ -41,9 +51,14 @@ function openjs()
     main.classList.add('main-open');
     second.classList.remove('openweb');
     first.classList.add('openjs');
+    third.classList.remove('opencontact')
+    fourth.classList.remove('openaboutme')
     change.classList.toggle("menu-active");
     menu.classList.remove("menu-icon-active");
-    select()
+    js.style.color="gray"
+    web.style.color="white"
+    aboutme.style.color="white"
+    contact.style.color="white"
 }
 
 function openhome()
@@ -51,19 +66,42 @@ function openhome()
     main.classList.remove('main-open');
     second.classList.remove('openweb');
     first.classList.remove('openjs');
+    third.classList.remove('opencontact')
+    fourth.classList.remove('openaboutme')
     change.classList.remove("menu-active");
     menu.classList.remove("menu-icon-active");
+    js.style.color="white"
+    web.style.color="white"
+    aboutme.style.color="white"
+    contact.style.color="white"
 }
 
-function select()
+function opencontact()
 {
-    switch (buttons){
-        case web:
-            if (first.classList.contains("openjs"))
-            {
-                web.classList.add("js-open")
-                alert('hola')
-            }
-        break;
-    }
+    main.classList.add('main-open');
+    first.classList.remove('openjs');
+    second.classList.remove('openweb');
+    third.classList.add('opencontact')
+    fourth.classList.remove('openaboutme')
+    change.classList.toggle("menu-active");
+    menu.classList.remove("menu-icon-active");
+    js.style.color="white"
+    web.style.color="white"
+    aboutme.style.color="white"
+    contact.style.color="gray"
+}
+
+function openaboutme()
+{
+    main.classList.add('main-open');
+    first.classList.remove('openjs');
+    second.classList.remove('openweb');
+    third.classList.remove('opencontact')
+    fourth.classList.add('openaboutme')
+    change.classList.toggle("menu-active");
+    menu.classList.remove("menu-icon-active");
+    js.style.color="white"
+    web.style.color="white"
+    aboutme.style.color="gray"
+    contact.style.color="white"
 }
